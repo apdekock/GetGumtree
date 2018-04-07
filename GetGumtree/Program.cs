88427236ecae220e53541bd9713acb4dc42ea001
philip <philipdekock@gmail.com>
2018-04-07 08:00:02 +0200

    SPA

--- GetGumtree/Program.cs
+++ GetGumtree/Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Linq;
 using Aggregator;
 using GitSharp;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Net.Http;
 using File = System.IO.File;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using Polly;
 
 namespace GetGumtree
 {
@@ -26,111 +30,129 @@ namespace GetGumtree
             //var arg5 = @"http://www.wesellcars.co.za/vehicle/category/all"      
             try
             {
-                StringBuilder listOfLines = new StringBuilder();
                 var chromeOptions = new ChromeOptions();
                 chromeOptions.AddArguments("-incognito");
+                chromeOptions.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+
                 using (IWebDriver driver = new ChromeDriver(Path.Combine(Directory.GetCurrentDirectory(), "WebDriverServer"), chromeOptions))
                 {
                     driver.Navigate().GoToUrl(args[5]);
-                    var findElement = driver.FindElements(By.CssSelector("#feed_1 > div > div.vehicles.grid > div.item"));
 
-                    foreach (var item in findElement)
+                    var scrapedItems = new List<ScrapeItem>();
+
+                    bool hasNextPage = true;
+
+                    while (hasNextPage)
                     {
-                        var image = item.FindElement(By.CssSelector("a"));
-                        var link = image.GetAttribute("href");
-                        var lines =
-                            new List<string>(item.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None))
+                        try
+                        {
+                            Policy
+                                .Handle<StaleElementReferenceException>()
+                                .WaitAndRetry(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                                    , (exception, timeSpan, retryAttempt) =>
+                                    {
+                                        Console.WriteLine("On " + timeSpan + ": " + exception.Message);
+                                    }).Execute(() =>
+                                        {
+                                            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                                            wait.Until(driver1 => (ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("Loader"))));
+                                            wait.Until(driver1 => (ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("Loader"))));
+                                            List<ScrapeItem> collection = ScrapePage(args, driver);
+                                            if (collection.Count == 0) { throw new StaleElementReferenceException("empty collection"); }
+                                            scrapedItems.AddRange(collection);
+                                        });
+
+                            hasNextPage = driver.FindElements(By.CssSelector(".ListNavigation_Next")).FirstOrDefault() != null;
+                            if (hasNextPage)
                             {
-                                link
-                            };
-                        var format = string.Join(",", lines);
-                        listOfLines.AppendLine(format);
-                        Console.WriteLine(format);
+                                Policy
+                                    .Handle<InvalidOperationException>()
+                                    .WaitAndRetry(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+                                        , (exception, timeSpan, retryAttempt) =>
+                                        {
+                                            Console.WriteLine("On " + timeSpan + ": " + exception.Message);
+                                        }).Execute(() =>
+                                            {
+                                                driver.FindElements(By.CssSelector(".ListNavigation_Next")).First().Click();
+                                                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                                                wait.Until(driver1 => (ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("Loader"))));
+                                                wait.Until(driver1 => (ExpectedConditions.InvisibilityOfElementLocated(By.ClassName("Loader"))));
+                                            });
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        Console.WriteLine(string.Format("Items scraped: {0}", scrapedItems.Count));
                     }
 
-                    sendNotification(string.Format("Scraped: {0} items from WeSellCars.", findElement.Count), 1, args[4]);
+                    //var dataAccess = new DataAccess(log);
+                    //var saveResult = dataAccess.SaveItems(items);
+                    sendNotification(string.Format("Scraped: {0} items from WeSellCars.", scrapedItems.Count), 1, args[4]);
                     driver.Quit();
-                }
-
-                var cTempCarsTxt = args[0] + @"\WeSellCars_" + DateTime.Now.ToString("dd_MM_yyyy_hh_mm_ss") + ".csv";
-                var fileStream = File.Create(cTempCarsTxt);
-                fileStream.Close();
-                File.WriteAllText(cTempCarsTxt, listOfLines.ToString());
-
-
-                StringBuilder postTemplate = new StringBuilder();
-                postTemplate.AppendLine("---");
-                postTemplate.AppendLine("layout: post ");
-                postTemplate.AppendLine("title: \"Scraping and GitSharp, and Spark lines\" ");
-                postTemplate.AppendLine("date: 2016-08-07");
-                postTemplate.AppendLine(
-                    "quote: \"If you get pulled over for speeding. Tell them your spouse has diarrhoea. — Phil Dunphy [Phil’s - osophy]\"");
-                postTemplate.AppendLine("categories: scraping, auto generating post, gitsharp");
-                postTemplate.AppendLine("---");
-                postTemplate.AppendLine(
-                    string.Format(
-                        "This page is a daily re-generated post (last re-generated  **{0}**), that shows the movement of prices on the [www.weSellCars.co.za](http://www.wesellcars.co.za) website.",
-                        DateTime.Now));
-                postTemplate.AppendLine("");
-                postTemplate.AppendLine("## Why?");
-                postTemplate.AppendLine("");
-                postTemplate.AppendLine(
-                    "This post is the culmination of some side projects I've been playing around with. Scraping, looking for a way to integrate with git through C# and a challenge to use this blog (which has no back-end or support for any server side scripting) to dynamically update a post. I realise that would best be accomplished through just making new posts but I opted for an altered post as this is a tech blog, and multiple posts about car prices would not be appropriate.");
-                postTemplate.AppendLine("");
-                postTemplate.AppendLine("# Lessons learned");
-                postTemplate.AppendLine("");
-                postTemplate.AppendLine(
-                    "* [GitSharp](http://www.eqqon.com/index.php/GitSharp) is limited and I needed to grab the project from [github](https://github.com/henon/GitSharp) in order to use it.");
-                postTemplate.AppendLine(
-                    "    The NuGet package kept on complaining about a **repositoryformatversion** setting in config [Core] that it required even though it was present, it still complained. So, I downloaded the source to debug the issue but then I did not encounter it. Apart from that - gitsharp did not allow me to push - and it seems the project does not have a lot of contribution activity (not criticising, just stating. I should probably take this up and contribute, especially as I would like to employ git as a file store for an application. Levering off the already refined functions coudl be a win but more on that in another post).");
-                postTemplate.AppendLine(
-                    "* Scraping with Selenium is probably not the best way - rather employ [HttpClient](https://msdn.microsoft.com/en-us/library/system.net.http.httpclient(v=vs.118).aspx).");
-                postTemplate.AppendLine(
-                    "* For quick, easy and painless sparklines [jQuery Sparklines](http://omnipotent.net/jquery.sparkline/#s-about)");
-                postTemplate.AppendLine(
-                    "* No backend required, just a simple process running on a server, that commits to a repo (ghPages) gets the job done.");
-
-                postTemplate.AppendLine("");
-                var aggregateData = new AggregateData(new FileSystemLocation(args[0]));
-                var dictionary = aggregateData.Aggregate();
-
-                var html = aggregateData.GetHTML(dictionary);
-                if (dictionary.Count > 0)
-                {
-                    postTemplate.AppendLine("## The List");
-                }
-                postTemplate.AppendLine(html);
-
-                // update post file
-                FileInfo fi = new FileInfo(args[1] + args[2]);
-                var streamWriter = fi.CreateText();
-                streamWriter.WriteLine(postTemplate.ToString());
-
-                streamWriter.Flush();
-                streamWriter.Dispose();
-
-                Repository repository = new Repository(args[1]);
-
-                repository.Index.Add(args[1] + args[2]);
-                Commit commited = repository.Commit(string.Format("Updated {0}", DateTime.Now),
-                    new Author("Philip de Kock", "philipdekock@gmail.com"));
-                if (commited.IsValid)
-                {
-                    string gitCommand = args[3];
-                    const string gitPushArgument = @"push origin";
-                    ProcessStartInfo psi = new ProcessStartInfo(gitCommand, gitPushArgument)
-                    {
-                        WorkingDirectory = args[1],
-                        UseShellExecute = true
-                    };
-                    Process.Start(psi);
                 }
             }
             catch (Exception e)
             {
-                sendNotification(string.Format("Scraper exception: {0}",e.Message), 5, args[4]);
+                sendNotification(string.Format("Scraper exception: {0}", e.Message), 5, args[4]);
                 Console.WriteLine(e.Message);
             }
+        }
+
+
+        private static List<ScrapeItem> ScrapePage(string[] args, IWebDriver driver)
+        {
+            var scrapedItems = new List<ScrapeItem>();
+
+            var findElementWrapper = driver.FindElements(By.CssSelector(".GalleryWrapper")).Last();
+            var findElements = findElementWrapper.FindElements(By.CssSelector(".GalleryItem"));
+
+            foreach (var item in findElements)
+            {
+                var title = string.Empty;
+                var Branch = string.Empty;
+                var Year = string.Empty;
+                var Price = string.Empty;
+                var Mileage = string.Empty;
+
+                var values = item.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+                title = values.First();
+
+                foreach (var value in values)
+                {
+                    if (value.StartsWith("Year:"))
+                    {
+                        Year = new string(value.Where(Char.IsDigit).ToArray());
+                    }
+                    const string BranchText = "Branch:";
+                    if (value.StartsWith(BranchText))
+                    {
+                        Branch = value.Remove(0, BranchText.Length);
+                    }
+                    if (value.Contains("Price:"))
+                    {
+                        Price = new string(value.Where(Char.IsDigit).ToArray());
+                    }
+                    if (value.StartsWith("Mileage:"))
+                    {
+                        Mileage = new string(value.Where(Char.IsDigit).ToArray());
+                    }
+                }
+
+                try
+                {
+                    var scrapedItem = new ScrapeItem(title, new Uri(args[5]), Year, Price, Mileage, Branch);
+                    scrapedItems.Add(scrapedItem);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            return scrapedItems;
         }
 
         private static void sendNotification(string message, int level, string secret)
---
+++ GetGumtree/ScrapeItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetGumtree
+{
+    public class ScrapeItem
+    {
+        public ScrapeItem(string title, Uri link, string year, string price, string mileage, string branch)
+        {
+            Title = title;
+            Link = link;
+            var outYear = 0;
+            if (int.TryParse(year, out outYear))
+                Year = outYear;
+            Branch = branch;
+            var outmileage = 0;
+            if (int.TryParse(mileage, out outmileage))
+                Mileage = outmileage;
+            var outPrice = 0;
+            if (int.TryParse(price, out outPrice))
+                Price = outPrice / 100;
+        }
+
+        public string Title { get; set; }
+        public Uri Link { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+        public int Mileage { get; set; }
+        public string Branch { get; set; }
+
+        public override string ToString()
+        {
+            return string.Join(",", new string[] { Title, Link.AbsolutePath });
+        }
+    }
+}
